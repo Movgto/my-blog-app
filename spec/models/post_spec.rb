6b68cdac818a120a6138ea32e9bc8e6974096d07
Gumaro Monroy Vazquez <maromvz@gmail.com>
2023-11-04 12:33:20 -0600

    Add remaining unit tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe Post, type: :model do
   subject { User.new(name: 'Tommy') }
   let(:post) { Post.new(author: subject, title: 'Post 1', text: 'Post content') }
+  let(:comment) { Comment.new(post: post, user: subject, text: 'This is a comment') }
 
   before do
     subject.save
     post.save
+    comment.save
   end
 
   it 'title should be present' do
@@ -27,5 +29,13 @@ describe Post, type: :model do
     post.comments_counter = -1
     post.likes_counter = -1
     expect(post).to_not be_valid
+  end
+
+  it 'returns first 3 recent comments' do
+    expect(post.recent_comments[0]).to eq(comment)
+  end
+
+  it 'updates user posts counter' do
+    expect(subject.posts_counter).to eq(1)
   end
 end
