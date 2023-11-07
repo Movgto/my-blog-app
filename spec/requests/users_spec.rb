fbfda746cd978b01b0793407e190cf75a88dbda7
Gumaro Monroy Vazquez <maromvz@gmail.com>
2023-11-07 16:35:26 -0600

    Fix linters

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-describe "GET /show" do
-  it "returns a successful response" do
-    user = User.create!(name: 'Timmy')
+describe 'GET /show' do
+  it 'returns a successful response' do
+    User.create!(name: 'Timmy')
     get users_url
     puts response.status
     expect(response).to be_successful
   end
 
   it 'renders a correct template' do
-    user = User.create!(name: 'Timmy')
+    User.create!(name: 'Timmy')
     get users_url
     puts response.status
     expect(response.body).to match(/This is a list of users/i)
